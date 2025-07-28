5eab01d768b70f0994dbb9d499b37e2ee686c738
jem3ii <ayoubjemai@takiacademyteam.com>
2025-07-28 09:22:06 +0100

    remove unused import

--- iecd-types/src/index.ts
+++ iecd-types/src/index.ts
@@ -9,30 +9,30 @@ export type { TMessageReactionTypeEnum } from "../../src/feature/messages/domain
 export { NOTIFICATION_TYPES_ENUM } from "../../src/features/notification/constants/constants";
 export type { TNotificationTypesEnum } from "../../src/features/notification/constants/constants";
 export type { PaginationMeta } from "../../src/newDatabase/mongo/types";
-export type { TEndUserEnum } from "./../../src/constants/globalEnums";
+export type { TEndUserEnum } from "../../src/constants/globalEnums";
 
 export {
   EXAM_GRADE_SYSTEM_ENUM,
   TExamGradeSystemEnum,
   ESTABLISHMENT_TITLE_ENUM,
   TEstablishmentTitleEnum,
-} from "./../../src/feature/levels/domains/level.entity";
-export { startConversationRules } from "./../../src/feature/messages/constants/conversation.constant";
+} from "../../src/feature/levels/domains/level.entity";
+export { startConversationRules } from "../../src/feature/messages/constants/conversation.constant";
 export {
   INSTANCE_TYPE_ENUM,
   TInstanceTypeEnum,
-} from "./../../src/feature/schools/domain/school.entity";
-export type { ReactionSummaryDTO } from "./../../src/feature/messages/dtos/Message.dto";
+} from "../../src/feature/schools/domain/school.entity";
+export type { ReactionSummaryDTO } from "../../src/feature/messages/dtos/Message.dto";
 export {
   FEATURE_FLAGS_ENUM,
   TFeatureFlagsEnum,
-} from "./../../src/feature/schools/constants/featureFlags";
+} from "../../src/feature/schools/constants/featureFlags";
 export {
   EDUCATION_DEPARTMENT_ENUM,
   GRADE_REPORT_THEM_ENUM,
   TEducationDepartmentEnum,
   TGradeReportThemEnum,
-} from "./../../src/feature/schools/domain/school.entity";
+} from "../../src/feature/schools/domain/school.entity";
 
 export * from "./autoExport/index";
 export type ID = string & { _isID: true };
