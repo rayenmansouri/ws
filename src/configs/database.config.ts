7be4d9e622c6e9a6329c40f90f49b0dfe2438b05
rayen mansouri <mansourirayen@takiacademyteam.com>
2025-08-05 13:51:32 +0100

    feat(database): configure MongoDB connection using environment variables for improved security and flexibility

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,4 +1,8 @@
-export const masterDabaseUri = `mongodb+srv://rayenmansouri:VsV35mzJ3UZdpkSk@cluster0.jgxndvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=master`;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const masterDabaseUri = `${process.env.MONGODB_URI}&dbName=master`;
 export const getDatabaseUri = (tenantId: string) => {
-    return `mongodb+srv://rayenmansouri:VsV35mzJ3UZdpkSk@cluster0.jgxndvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&dbName=${tenantId}`;
+    return `${process.env.MONGODB_URI}&dbName=${tenantId}`;
 };
