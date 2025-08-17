b118c6d5dfed3abe0d9336605aed7f69d6ff5814
rayen mansouri <mansourirayen@takiacademyteam.com>
2025-08-17 16:51:09 +0100

    refactor(uploadAvatar): replace authorized middlewares with core middlewares

--- src/api-v2/user-management/uploadAvatar/uploadAvatar.route.ts
+++ src/api-v2/user-management/uploadAvatar/uploadAvatar.route.ts
@@ -1,8 +1,8 @@
 import { publicRouter } from "../../../apps/main/routers/public-router";
-import { getAuthorizedMiddlewares } from "../../../core/express/middlewares/registerCoreMiddlewares";
 import { RouteConfiguration } from "../../../core/express/types";
 import { UploadAvatarController } from "./uploadAvatar.controller";
 import { UploadAvatarRouteConfig } from "./uploadAvatar.types";
+import { getCoreMiddlewares } from "../../../core/express/middlewares/registerCoreMiddlewares";
 
 export const uploadAvatarRoute: RouteConfiguration<UploadAvatarRouteConfig, "/avatar"> = {
     path: "/avatar",
@@ -10,9 +10,9 @@ export const uploadAvatarRoute: RouteConfiguration<UploadAvatarRouteConfig, "/av
     endUser: "ADMIN",
     controller: UploadAvatarController,
     isTransactionEnabled: false,
-    middlewaresClasses: getAuthorizedMiddlewares(),
     platform: "web",
     router: publicRouter,
+    middlewaresClasses: getCoreMiddlewares(),
     upload: { 
         fields: [{ name: "avatar", maxCount: 1 }] 
     }
