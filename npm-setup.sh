5e4f3d1e41f45d420c28f45c4fae0599d9a565bf
rayen mansouri <mansourirayen@takiacademyteam.com>
2025-08-19 14:43:46 +0100

    chore(ci): streamline GitLab CI configuration by removing npm setup script, updating package.json scripts, and adding git config commands

--- npm-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npm config set -- //gitlab.com/api/v4/projects/72058675/packages/npm/:_authToken=glpat-Ppl80lbWbfzUX7EQQNclYm86MQp1OmhwOTRlCw.01.12181yt04
-npm config set @Softylines-Alternance:registry=https://gitlab.com/api/v4/projects/72058675/packages/npm/
- 
